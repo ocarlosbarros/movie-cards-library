fde78f1010c73531c1fca4ea557afebe96f24015
Carlos Barros <carlos_dbs@hotmail.com>
2021-09-01 17:30:03 -0300

    Adiciona subtitle ao movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,6 +8,7 @@ class MovieCard extends Component {
       <div>
         <img src={ movie.imagePath } alt={ movie.storyline } />
         <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
       </div>
     );
   }
