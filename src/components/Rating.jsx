da340c0580de345667d7516c8a58208f01d29122
Carlos Barros <carlos_dbs@hotmail.com>
2021-09-01 17:48:40 -0300

    Adiciona Rating ao MovieCard

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,11 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return (
+      <p>rating</p>
+    );
+  }
+}
+
+export default Rating;
