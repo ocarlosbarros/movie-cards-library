3d62fb50f8116cbbf83fcce4494d51b5e2445140
Carlos Barros <carlos_dbs@hotmail.com>
2021-09-01 23:07:18 -0300

    Adiciona position fixed no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header>
+      <header className="header-page">
         <h1>Movie Cards Library</h1>
       </header>
     );
